c2bb641cb651d00b630f87ef753ea56eb04f3768
Cromuel Oliver <cromuelbarut@gmail.com>
2023-01-28 18:15:36 +0800

    created failure class

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final List<dynamic> properties;
+
+  const Failure([
+    this.properties = const <dynamic>[],
+  ]);
+
+  @override
+  List<Object> get props => [properties];
+
+  @override
+  String toString() => 'Failure(properties: $properties)';
+}
