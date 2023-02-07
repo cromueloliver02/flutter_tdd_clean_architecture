cbd21d37ca39c6a6ab72eb85b496b0a11be40ebb
Cromuel Oliver <cromuelbarut@gmail.com>
2023-02-07 23:16:35 +0800

    changed expected type of data in number trivia model test

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entities/number_trivia_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
@@ -9,7 +10,7 @@ main() {
     'should be a subclass of NumberTrivia entity',
     () async {
       // assert
-      expect(tNumberTriviaModel, isA<NumberTriviaModel>());
+      expect(tNumberTriviaModel, isA<NumberTrivia>());
     },
   );
 }
