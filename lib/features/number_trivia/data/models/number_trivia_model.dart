91ecd67a1a40de4615d6331935c697533f123ba6
Cromuel Oliver <cromuelbarut@gmail.com>
2023-02-07 23:13:58 +0800

    created NumberTriviaModel and extended it from NumberTrivia entity and created its test

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,8 @@
+import '../../domain/entities/number_trivia_entity.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  const NumberTriviaModel({
+    required super.number,
+    required super.text,
+  });
+}
---
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
+
+main() {
+  const tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'test');
+
+  test(
+    'should be a subclass of NumberTrivia entity',
+    () async {
+      // assert
+      expect(tNumberTriviaModel, isA<NumberTriviaModel>());
+    },
+  );
+}
